8897494097e97368cdf8f4cfcfb4c45d3ce4a881
JAVIER RODRIGUEZ B <j.rodriguez@gooddatamx.onmicrosoft.com>
2021-04-14 20:00:18 -0500

    Cambiar tipos de datos MedicalTest

--- ExpedienteIDON/Models/MedicalTest.cs
+++ ExpedienteIDON/Models/MedicalTest.cs
@@ -8,16 +8,16 @@ namespace ExpedienteIDON.Models
     public class MedicalTest
     {
         public int Id { get; set; }
-        public string Radiograph { get; set; }
+        public bool Radiograph { get; set; }
         public DateTime RadiographDate { get; set; }
-        public string RadiographNormal { get; set; }
-        public string Ultrasound { get; set; }
+        public bool RadiographNormal { get; set; }
+        public bool Ultrasound { get; set; }
         public DateTime UltrasoundDate { get; set; }
         public string UltrasoundType { get; set; }
         public string Observations { get; set; }
-        public string Electrocardiography { get; set; }
+        public bool Electrocardiography { get; set; }
         public DateTime ElectrocardiographyDate { get; set; }
-        public string ElectrocardiographyNormal { get; set; }
+        public bool ElectrocardiographyNormal { get; set; }
         public string ElectrocardiographyObservations { get; set; }
         public string Others { get; set; }
 

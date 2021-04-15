332b062c975fd2b874903b0c534f89579357f83f
JAVIER RODRIGUEZ B <j.rodriguez@gooddatamx.onmicrosoft.com>
2021-04-15 12:36:47 -0500

    Update HistoryRecordViewModel con las nuevas clases

--- ExpedienteIDON/ViewModels/HistoryRecordViewModel.cs
+++ ExpedienteIDON/ViewModels/HistoryRecordViewModel.cs
@@ -16,6 +16,19 @@ namespace ExpedienteIDON.ViewModels
         public PathologicRecord PathologicRecord { get; set; }
         public OtherPathologicRecord OtherPathologicRecord { get; set; }
         public MedicalRecord MedicalRecord { get; set; }
+        public Symptom Symptom { get; set; }
+        public VitalSigns VitalSigns { get; set; }
+        public PhysicalExploration PhysicalExploration { get; set; }
+        public MedicalTest MedicalTest { get; set; }
+        public LabsTest LabsTest { get; set; }
+        public Others Others { get; set; }
+        public BiometriaHematica BiometriaHematica { get; set; }
+        public QuimicaSanguinea QuimicaSanguinea { get; set; }
+        public Hormonas Hormonas { get; set; }
+        public PerfilHepatico PerfilHepatico { get; set; }
+        public PerfilTiroideo PerfilTiroideo { get; set; }
+        public GeneralOrina GeneralOrina { get; set; }
+        public OtrosLabs OtrosLabs { get; set; }
         public IEnumerable<BloodType> BloodTypes { get; set; }
 
     }
